dd72b65b08febf63f2e9328dbea19340a161a5db
Luis Arteiro <lucho14garfield@gmail.com>
2020-04-25 22:32:29 +0100

    Fixing error handler
    Fixing "undefined" response from the server.

--- templates/app.js
+++ templates/app.js
@@ -37,7 +37,7 @@ app.use(cookieParser());
 app.use('/users', usersRouter);
 
 // error handler
-app.use(function (err, req, res) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
